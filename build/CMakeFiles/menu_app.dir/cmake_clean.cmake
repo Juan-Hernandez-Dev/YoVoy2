f4e5026dec49ddc955dacf023849983723e3c679
Juan Hernandez <juan.h.developer@gmail.com>
2025-12-01 23:39:04 -0600

    implement cool as hell dynamic menu

---
+++ build/CMakeFiles/menu_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menu_app.dir/main.cpp.obj"
+  "CMakeFiles/menu_app.dir/main.cpp.obj.d"
+  "libmenu_app.dll.a"
+  "menu_app.exe"
+  "menu_app.exe.manifest"
+  "menu_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/menu_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
